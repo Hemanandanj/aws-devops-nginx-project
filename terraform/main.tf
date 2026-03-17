f959364c97eadc7a85344d3d1d3ee94932981be3
Hemanandanj <128038793+Hemanandanj@users.noreply.github.com>
2026-03-17 12:00:40 +0530

    Refactor AWS instances and add lifecycle rule
    Updated instance type and resource names for AWS instances. Added lifecycle rule to prevent destruction of the my_web_server resource.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,12 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAQGCQDPPADU4PD2JO"
-  secret_key = "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"
-}
-
-resource "aws_instance" "web1" {
-  ami           = "ami-03caad32a158f72db"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "web-server-1"
-  }
-}
-
-resource "aws_instance" "web2" {
-  ami           = "ami-03caad32a158f72db"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "web-server-2"
-  }
-}
-
-resource "aws_instance" "nginx_lb" {
+resource "aws_instance" "my_web_server" {
   ami           = "ami-03caad32a158f72db"
   instance_type = "t3.small"
 
   tags = {
     Name = "nginx-lb"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }

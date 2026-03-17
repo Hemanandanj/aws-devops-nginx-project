72d068bf685e82a2d7c8b2197830d90b09765c1f
Hemanandanj <128038793+Hemanandanj@users.noreply.github.com>
2026-03-17 12:01:55 +0530

    Configure AWS provider in main.tf
    Added AWS provider configuration with region and credentials.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAQGCQDPPADU4PD2JO"
+  secret_key = "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"
+}
 resource "aws_instance" "my_web_server" {
   ami           = "ami-03caad32a158f72db"
   instance_type = "t3.small"

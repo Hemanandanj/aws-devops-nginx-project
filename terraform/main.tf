7ee53816af22399b025d2366e9405ad8ed37198c
Hemanandanj <128038793+Hemanandanj@users.noreply.github.com>
2026-03-17 12:34:23 +0530

    Refactor AWS instance resources in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,30 @@ provider "aws" {
   access_key = "AKIAQGCQDPPADU4PD2JO"
   secret_key = "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"
 }
-resource "aws_instance" "my_web_server" {
+
+resource "aws_instance" "web1" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "nginx_lb" {
   ami           = "ami-03caad32a158f72db"
   instance_type = "t3.small"
 
   tags = {
     Name = "nginx-lb"
-  }
-
-  lifecycle {
-    prevent_destroy = true
   }
 }

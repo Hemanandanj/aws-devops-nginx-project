6f2530cd9cad2bf1a4edf5d0b0b3335b075b25af
Hemanandanj <128038793+Hemanandanj@users.noreply.github.com>
2026-03-17 11:09:35 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,30 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_instance" "web1" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "nginx_lb" {
+  ami           = "ami-03caad32a158f72db"
+  instance_type = "t3.small"
+
+  tags = {
+    Name = "nginx-lb"
+  }
+}
